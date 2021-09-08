3ec3005690245a8d78ee4ba6a66e254d08c9d68e
Oliver <oliverdesposito@gmail.com>
2021-09-08 12:41:20 -0400

    init commit add intro lesson

---
+++ p1-intro_to_python/dice.py
@@ -0,0 +1,32 @@
+import random
+
+def role_n_sides_n_times(sides, roles):
+    #roles a n sided dice for n roles takes (sides, roles)
+    roles_list = []
+
+    for roles in range(roles):
+        role = random.randint(1, sides)
+        roles_list.append(role)
+    
+    return roles_list
+
+def role_one(sides = 6):
+    return random.randint(1, sides)
+
+def role_w_mod(sides = 20, mod = 1):
+    role = random.randint(1, sides) + mod
+    return role
+
+def role_n_sides_dif(sides):
+    ops = list(range(1, sides + 1))
+    roles_list = []
+
+    while(len(ops) != 0):
+        role = random.choice(ops)
+        roles_list.append(role)
+        ops.remove(role)
+
+    return roles_list
+
+roles = role_one(4) 
+print(roles)
